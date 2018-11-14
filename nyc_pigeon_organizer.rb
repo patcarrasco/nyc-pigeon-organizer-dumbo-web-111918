8c3102872c13230be14684ee4534c9d11fff28e3
github username <github email address>
2018-11-14 14:52:43 -0500

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,31 @@
+
+require 'pry'
+
 def nyc_pigeon_organizer(data)
   # write your code here!
+
+  new = {}
+
+  # POPULATE KEYS
+  data[:gender].each do |s, names|
+    names.each { |name| new[name] = {} }
+  end
+
+  #POPULATE VALUES
+  # binding.pry
+
+  new.each do |name, info|
+    data.each do |cat, v|
+      new[name][cat] = []
+      v.each do |k, val|
+        if val.include?(name)
+          new[name][cat].push(k.to_s)
+        end
+      end
+    end
+    # binding.pry
+  end
+
+  new
+
 end
